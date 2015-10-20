035128f9bb87e48fdf5742b0e2b24e3edc0e7f82
jmithani <jazzmithani@gmail.com>
2015-10-20 13:59:56 -0500

    solved jquery error, have basic quiz frame set up

--- quiz/EE-test.js
+++ quiz/EE-test.js
@@ -1,16 +1,16 @@
 $(document).ready(function() 
 {    $("#results").click(function() {                
 
-if (!$("input[@name=q1]:checked").val() ||            
-!$("input[@name=q2]:checked").val() ||            
-!$("input[@name=q3]:checked").val() ||            
-!$("input[@name=q4]:checked").val() ||            
-!$("input[@name=q5]:checked").val() ||            
-!$("input[@name=q6]:checked").val() ||            
-!$("input[@name=q7]:checked").val() ||            
-!$("input[@name=q8]:checked").val() ||            
-!$("input[@name=q9]:checked").val() ||            
-!$("input[@name=q10]:checked").val()            
+if (!$("input[name=q1]:checked").val() ||            
+!$("input[name=q2]:checked").val() ||            
+!$("input[name=q3]:checked").val() ||            
+!$("input[name=q4]:checked").val() ||            
+!$("input[name=q5]:checked").val() ||            
+!$("input[name=q6]:checked").val() ||            
+!$("input[name=q7]:checked").val() ||            
+!$("input[name=q8]:checked").val() ||            
+!$("input[name=q9]:checked").val() ||            
+!$("input[name=q10]:checked").val()            
 ) {            
 alert("You're not done yet!");        
 }        
@@ -29,25 +29,25 @@ var cat10name = "10";
 var cat11name = "None";            
             
 
-var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
+var cat1 = ($("input[name=q1]:checked").val() != "a"); 
            
-var cat2 = ($("input[@name=q2]:checked").val() != "b");  
+var cat2 = ($("input[name=q2]:checked").val() != "b");  
 
-var cat3 = ($("input[@name=q3]:checked").val() != "c");  
+var cat3 = ($("input[name=q3]:checked").val() != "c");  
 
-var cat4 = ($("input[@name=q4]:checked").val() != "d");  
+var cat4 = ($("input[name=q4]:checked").val() != "d");  
 
-var cat5 = ($("input[@name=q5]:checked").val() != "a"); 
+var cat5 = ($("input[name=q5]:checked").val() != "a"); 
 
-var cat6 = ($("input[@name=q6]:checked").val() != "b");  
+var cat6 = ($("input[name=q6]:checked").val() != "b");  
 
-var cat7 = ($("input[@name=q7]:checked").val() != "c"); 
+var cat7 = ($("input[name=q7]:checked").val() != "c"); 
 
-var cat8 = ($("input[@name=q8]:checked").val() != "d");  
+var cat8 = ($("input[name=q8]:checked").val() != "d");  
 
-var cat9 = ($("input[@name=q9]:checked").val() != "a"); 
+var cat9 = ($("input[name=q9]:checked").val() != "a"); 
 
-var cat10 = ($("input[@name=q10]:checked").val() != "b");  
+var cat10 = ($("input[name=q10]:checked").val() != "b");  
 
 var cat11 = (!cat1 && !cat2 && !cat3 && !cat4 && !cat5 && !cat6 && !cat7 && !cat8 && !cat9 && !cat10); var categories = [];                        
 
