c4fa62967f5721586ec6bb58e9e5a661f695dbb8
jmithani <jazzmithani@gmail.com>
2015-11-17 11:46:02 -0600

    updated with contract for ans function

--- quiz/EE-test.js
+++ quiz/EE-test.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-	var choice1 = 'The number of American teen pregnancies each year';
-  var choice2 = [
+	var choice1Text = [
+    'The number of American teen pregnancies each year'
+    ];
+  var choice2Text = [
     {msg: 'Number of active US Army personnel', ans: 2},
     {msg: 'Total stars visible from earth', ans: 2},
     {msg: 'General surgeons in the US', ans: 2},
     {msg: 'Total teen girls in US', ans: 1}
     ];
 
-  var answerKey = {1, 1, 1, 2};
+  var answerKey = [1, 1, 1, 2];
   var questionProgress = 0;
 
-  document.getElementById('choice1').innerHTML = choice1[0];
+  document.getElementById('choice1').innerHTML = choice1Text[0];
+  document.getElementById('choice2').innerHTML = choice2Text[questionProgress].msg;
+  //$('choice1').html(choice1Text[0]);
 
   $("#choice1").click(function() {
-    if (choice2[questionProgress].ans === 2) {
+    if (choice2Text[questionProgress].ans === 2) {
       $("#choice1").css({
         'background-color': 'red'
       });
@@ -28,7 +32,7 @@ $(document).ready(function() {
   });
 
   $("#choice2").click(function() {
-    if (choice2[questionProgress].ans === 2) {
+    if (choice2Text[questionProgress].ans === 2) {
       $("#choice2").css({
         'background-color': 'green'
       });
@@ -38,89 +42,17 @@ $(document).ready(function() {
 
   });
 
-};
+});
 
+// okay, what do we want to do?
+// i want to check if what was clicked was the correct answer
+// inputs: what was clicked / index
+// check against what was clicked against choice2Text[index].ans
+// if ===, turn green and increase index
+// if !, turn red, turn other green, and increase index
 
+// iterate this throughout the choice2Text array
 
-// {    $("#results").click(function() {                
+// $('#choice1').click
 
-// if (!$("input[name=q1]:checked").val() ||            
-// !$("input[name=q2]:checked").val() ||            
-// !$("input[name=q3]:checked").val() ||            
-// !$("input[name=q4]:checked").val() ||            
-// !$("input[name=q5]:checked").val() ||            
-// !$("input[name=q6]:checked").val() ||            
-// !$("input[name=q7]:checked").val() ||            
-// !$("input[name=q8]:checked").val() ||            
-// !$("input[name=q9]:checked").val() ||            
-// !$("input[name=q10]:checked").val()            
-// ) {            
-// alert("You're not done yet!");        
-// }        
-
-// else {            
-// var cat1name = "1";            
-// var cat2name = "2";            
-// var cat3name = "3";            
-// var cat4name = "4";            
-// var cat5name = "5";            
-// var cat6name = "6";            
-// var cat7name = "7";            
-// var cat8name = "8";            
-// var cat9name = "9";            
-// var cat10name = "10";            
-// var cat11name = "None";            
-            
-
-// var cat1 = ($("input[name=q1]:checked").val() != "a"); 
-           
-// var cat2 = ($("input[name=q2]:checked").val() != "b");  
-
-// var cat3 = ($("input[name=q3]:checked").val() != "c");  
-
-// var cat4 = ($("input[name=q4]:checked").val() != "d");  
-
-// var cat5 = ($("input[name=q5]:checked").val() != "a"); 
-
-// var cat6 = ($("input[name=q6]:checked").val() != "b");  
-
-// var cat7 = ($("input[name=q7]:checked").val() != "c"); 
-
-// var cat8 = ($("input[name=q8]:checked").val() != "d");  
-
-// var cat9 = ($("input[name=q9]:checked").val() != "a"); 
-
-// var cat10 = ($("input[name=q10]:checked").val() != "b");  
-
-// var cat11 = (!cat1 && !cat2 && !cat3 && !cat4 && !cat5 && !cat6 && !cat7 && !cat8 && !cat9 && !cat10); var categories = [];                        
-
-// if (cat1) { categories.push(cat1name) };            
-// if (cat2) { categories.push(cat2name) };            
-// if (cat3) { categories.push(cat3name) };            
-// if (cat4) { categories.push(cat4name) };            
-// if (cat5) { categories.push(cat5name) };            
-// if (cat6) { categories.push(cat6name) };            
-// if (cat7) { categories.push(cat7name) };            
-// if (cat8) { categories.push(cat8name) };            
-// if (cat9) { categories.push(cat9name) };            
-// if (cat10) { categories.push(cat10name) };            
-// if (cat11) { categories.push(cat11name) };                        
-
-// var catStr = 'You answered the following questions incorrectly: ' + categories.join(', ') + '';                     
-// $("#categorylist").text(catStr);                        
-// $("#categorylist").show("slow");            
-
-// if (cat1) { $("#category1").show("slow"); };            
-// if (cat2) { $("#category2").show("slow"); };            
-// if (cat3) { $("#category3").show("slow"); };            
-// if (cat4) { $("#category4").show("slow"); };            
-// if (cat5) { $("#category5").show("slow"); };            
-// if (cat6) { $("#category6").show("slow"); };            
-// if (cat7) { $("#category7").show("slow"); };            
-// if (cat8) { $("#category8").show("slow"); };            
-// if (cat9) { $("#category9").show("slow"); };            
-// if (cat10) { $("#category10").show("slow"); };            
-// if (cat11) { $("#category11").show("slow"); };
-// { $("#closing").show("slow"); };
-// }
-//     });});
+var checkIfCorrect = function()
