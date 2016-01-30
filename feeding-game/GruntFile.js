7833fc5b4ce8d96742cc0ceeadbaf089fa48b43e
Jonathan Uy <jonxuy@gmail.com>
2016-01-30 17:47:09 -0500

    grunt clean fix for windows Previously it didn't seem like grunt-clean was able to find the path to clean which then for whatever reason would lead to later grunt tasks like copy, concat and watch from working

--- feeding-game/GruntFile.js
+++ feeding-game/GruntFile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: [
-      'deploy'
+      './deploy'
     ],
     connect: {
       server: {
